aad918bf1f7d1012d1296137484f206649c63338
Euan Goddard <euan.goddard@gmail.com>
2018-12-14 20:02:21 +0000

    Added some more levels

--- src/app/levels/levels.definition.ts
+++ src/app/levels/levels.definition.ts
@@ -10,6 +10,8 @@ export const LEVELS: ReadonlyArray<Level> = [
   Level.parse('GPG\nPSP\nGPG'),
   Level.parse('P-P\n-G-\n-G-\n-G-\nPGS'),
   Level.parse('P-P\n-G-\n-P-\n-G-\nPGS'),
+  Level.parse('S---\n-GP-\n-PG-\n----'),
+  Level.parse('S--P\n-PP-\n-PP-\nP--G'),
   Level.parse('S--P\nGGG-\nP--P\n-GGG\nP--P'),
   Level.parse('--P--\n-PGP-\nS-G-P'),
   Level.parse('-G---G-\nG--G--G\nS-G-G-P'),
