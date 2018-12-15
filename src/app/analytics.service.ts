5d9743d61a522c72119a4ed5d6bc3d690ecb2ccd
Euan Goddard <euan.goddard@gmail.com>
2018-12-15 08:08:31 +0000

    Fixed analytics tracking code

--- src/app/analytics.service.ts
+++ src/app/analytics.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 declare global {
   interface Window {
-    dataLayer?: any[];
+    gtag: Function;
   }
 }
 
@@ -10,15 +10,8 @@ declare global {
   providedIn: 'root',
 })
 export class AnalyticsService {
-  constructor() {
-    this.addScript();
-    window.dataLayer = window.dataLayer || [];
-    this.gtag('js', new Date());
-    this.gtag('config', 'UA-131047329-1', { send_page_view: false });
-  }
-
   sendPageView(urlPath: string): void {
-    this.gtag('config', 'UA-131047329-1', {
+    window.gtag('config', 'UA-131047329-1', {
       page_path: urlPath,
       page_title: document.title,
       page_location: window.location.href,
@@ -26,20 +19,7 @@ export class AnalyticsService {
   }
 
   sendEvent(action: string, params: GtagEvent): void {
-    this.gtag('event', action, params);
-  }
-
-  private gtag(...args: any[]): void {
-    window.dataLayer!.push(args);
-  }
-
-  private addScript(): void {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-131047329-1';
-    const first = document.getElementsByTagName('script')[0]!;
-    first.parentNode!.insertBefore(script, first);
+    window.gtag('event', action, params);
   }
 }
 
